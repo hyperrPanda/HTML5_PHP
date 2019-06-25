a4c19c438e78b145a67d6062449630a4bee42acc
hyperrPanda <52204468+hyperrPanda@users.noreply.github.com>
2019-06-25 23:08:31 +1000

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
-	$host = "feenix-mariadb.swin.edu.au";
-	$user = "s102019464"; // your user name
-	$pwd = "210292"; // your password (date of birth ddmmyy unless changed)
-	$sql_db = "s102019464_db"; // your database
+	$host = "feenix-mariadb.gg";
+	$user = "xcvcv"; // your user name
+	$pwd = "sdeveloper"; // your password (date of birth ddmmyy unless changed)
+	$sql_db = "iop_ghyt"; // your database
 ?>
