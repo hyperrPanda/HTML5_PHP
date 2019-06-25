bd6c53043d34c25a2dd819ff5f01b1b0c57a69c7
hyperrPanda <52204468+hyperrPanda@users.noreply.github.com>
2019-06-25 23:15:25 +1000

    Add files via upload

---
+++ settingss - Copy.php
@@ -0,0 +1,6 @@
+<?php
+	$host = "feenix-mariadb";
+	$user = "uuuu"; // your user name
+	$pwd = "xxxxxx"; // your password (date of birth ddmmyy unless changed)
+	$sql_db = "oowqwq"; // your database
+?>
