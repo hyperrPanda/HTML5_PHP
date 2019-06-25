85775c4d39d7c0e4f027dca42fcac6c1edd6e64f
hyperrPanda <52204468+hyperrPanda@users.noreply.github.com>
2019-06-25 23:12:55 +1000

    Delete settings.php

--- settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$host = "feenix-mariadb.gg";
-	$user = "xcvcv"; // your user name
-	$pwd = "sdeveloper"; // your password (date of birth ddmmyy unless changed)
-	$sql_db = "iop_ghyt"; // your database
-?>
